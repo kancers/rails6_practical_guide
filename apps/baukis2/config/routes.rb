4c801e8e6a1cc6c46294c8fafb4315a532dd2094
kancers <kamba.ryosuke@lancers.co.jp>
2021-02-27 15:27:24 +0900

    Section 7.3.2

--- apps/baukis2/config/routes.rb
+++ apps/baukis2/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :staff do
     root 'top#index'
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create" as: :session
+    post "session" => "sessions#create", as: :session
     delete "session" => "sessions#destroy"
   end
   namespace :admin do
