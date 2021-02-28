2d1e634ed580843ed353481278218adcf3c7e16a
kancers <kamba.ryosuke@lancers.co.jp>
2021-02-28 13:31:24 +0900

    fix

--- apps/baukis2/app/models/staff_member.rb
+++ apps/baukis2/app/models/staff_member.rb
@@ -2,7 +2,7 @@ class StaffMember < ApplicationRecord
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
-    raw_password.nil?
+    elsif raw_password.nil?
       self.hashed_password = nil
     end
   end
